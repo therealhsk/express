c3baa9fec03971df29e84e97f02fc5a6b967b225
therealhsk <sheikh.hsk@gmail.com>
2020-02-01 18:17:36 +0500

    passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var logger = require('morgan');
 const mongoose = require('mongoose')
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,12 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
   name: 'session-id',
@@ -29,51 +37,25 @@ app.use(session({
   store: new FileStore()
 }));
 
-function auth(req, res, next) {
+app.use(passport.initialize());
+app.use(passport.session());
 
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      req.session.user = 'admin';
-      next(); // authorized
-    } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'admin') {
-      console.log('req.session: ', req.session);
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
-    }
+        next();
   }
 }
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('12345-67890-09876-54321'));
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
