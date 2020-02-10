74c8c7f0f2ee55d1e126ef35e27172aed9cdb440
therealhsk <sheikh.hsk@gmail.com>
2020-02-10 18:28:09 +0500

    File upload

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter')
-var promoRouter = require('./routes/promoRouter')
-var leaderRouter = require('./routes/leaderRouter')
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -54,6 +55,7 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
